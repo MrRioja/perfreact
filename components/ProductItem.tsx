bb4213cb52cf47b52f610859aa6ce4aaa42f17c9
Luiz Rioja <lulyrioja@gmail.com>
2022-08-15 00:14:09 -0300

    perf: add lazy loading

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,19 +1,43 @@
-import { memo } from "react";
+import dynamic from "next/dynamic";
+import { memo, useState } from "react";
+import { AddProductToWishlistProps } from "./AddProductToWishlist";
+
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(
+  () => {
+    return import("./AddProductToWishlist").then(
+      (mod) => mod.AddProductToWishlist
+    );
+  },
+  {
+    loading: () => <span>Carregando...</span>,
+  }
+);
 
 interface ProductItemProps {
   product: {
     id: number;
     price: number;
+    priceFormatted: number;
     title: string;
   };
   onAddToWishList: (id: number) => void;
 }
 
 function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
+  const [isAddingToWishlist, setIsAddingToWishlist] = useState(false);
+
   return (
     <div>
-      {product.title} - <strong>{product.price}</strong>
-      <button onClick={() => onAddToWishList(product.id)}>Favoritar</button>
+      {product.title} - <strong>{product.priceFormatted}</strong>
+      <button onClick={() => setIsAddingToWishlist(true)}>
+        Adicionar aos favoritos
+      </button>
+      {isAddingToWishlist && (
+        <AddProductToWishlist
+          onAddToWishlist={() => onAddToWishList(product.id)}
+          onRequestClose={() => setIsAddingToWishlist(false)}
+        />
+      )}
     </div>
   );
 }
